f91689e7e6fddf1cb206196ff2789f4b6a2453d2
ejp2004 <ericpetruzzelli04@gmail.com>
2026-05-12 23:20:13 -0400

    Added Main and debug. Basically basic starting stuff

--- FinanceMain.java
+++ FinanceMain.java
@@ -1,4 +1,15 @@
 public class FinanceMain 
 {
-    
+    //debuggers
+    static boolean debug=true;
+    public static void main(String[] args) 
+    {
+        if(debug)
+            System.out.println("DEBUG ON \n STARTING MAIN");
+    }
+
+    static void ReadFile(String file)
+    {
+        
+    }
 }
