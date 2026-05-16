80877373ab4bed05f7039098b37d619f2316ff7f
ejp2004 <ericpetruzzelli04@gmail.com>
2026-05-15 22:43:57 -0400

    Finished ReadFile

--- FinanceMain.java
+++ FinanceMain.java
@@ -3,33 +3,41 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Set;
 
 public class FinanceMain 
 {
     //SEVERAL HASHMAP OF SORTED SETS OF TRANSACTIONS!!!
-     Map<String, SortedSet<Transaction>> typeMap=new HashMap<>();
-
-
-
-
-
-
-
+     static Map<String, SortedSet<Transaction>> TypeMap=new HashMap<>();
+     static Map<String, SortedSet<Transaction>> LocationMap=new HashMap<>();
+     static Map<String, SortedSet<Transaction>> OriginMap=new HashMap<>();
 
 
     final public static String INFO_FILE_NAME="FinanceInfo.txt";
     //debuggers
     static boolean debug=true;
+    static boolean debugDetail=false;
     public static void main(String[] args) 
     {
         if(debug)
             System.out.println("DEBUG ON \n STARTING MAIN");
 
         ReadInfo(INFO_FILE_NAME);
+    
+
     }
 
     static void ReadInfo(String fileName)
     {
+        int[] curDate=new int[3];
+        Double curCost;
+        String curOrigin;
+        String curType;
+        String curActualLocation;
+        String curDescription; 
+
+        Transaction curTransaction;
         if(debug)
             System.out.println("STARTING READINFO");
 /*
@@ -51,16 +59,47 @@ Description: (nail polish, T-Shirt, concert tickets, etc)
         
         for (int i=0; i<amountToSkip; i++)
             scanner.nextLine();
-        System.out.println(scanner.next());
         
-        scanner.close();
+        
 
-        //saving dates
+        while(scanner.hasNextInt())
+        {
+           curDate[0]= scanner.nextInt();
+           curDate[1]=scanner.nextInt();
+           curDate[2]= scanner.nextInt();
+
+            curCost=scanner.nextDouble(); //ERROR HERE
+            scanner.nextLine();
+            curOrigin=scanner.nextLine();
+            curType=scanner.nextLine();
+            curActualLocation=scanner.nextLine();
+            curDescription= scanner.nextLine();
+
+           curTransaction=new Transaction(curDate, curCost, curOrigin, curType, curActualLocation, curDescription);
+           if(debugDetail)
+           {
+            System.err.println(curTransaction+"\n");
+           }
+            mapPut(TypeMap, curType, curTransaction);
+            mapPut(LocationMap, curActualLocation, curTransaction);
+            mapPut(OriginMap, curOrigin, curTransaction);
+        }
 
 
 
         if(debug)
             System.out.println("COMPLETED READINFO");
+        scanner.close();
+    }
+
+    public static void mapPut(Map<String, SortedSet<Transaction>> map, String key, Transaction value)
+    {
+
+        if(map.get(key)==null)
+        {
+            map.put(key,new TreeSet<Transaction>());
+        }
+        map.get(key).add(value);
     }
 
 }
--- Transaction.java
+++ Transaction.java
@@ -10,6 +10,7 @@ public class Transaction implements Comparable<Transaction>
     {
         this.date=date;
         this.cost=cost;
+        this.type=type;
         this.typeOrigin=typeOrigin;
         this.location=location;
         this.description=description;
@@ -30,7 +31,25 @@ public class Transaction implements Comparable<Transaction>
     @Override
     public int compareTo(Transaction other)
     {
-        if(date[)
-        return -1;
+        int index=0;
+        if(date[2]==other.date[2])
+        {
+            if(date[1]==other.date[1])
+            {
+                if(date[0]==other.date[0])
+                {
+                    return 0;
+                }
+                else
+                    index=0;
+            }
+            else
+                index=1;
+        }
+        else
+        {
+            index=2;
+        }
+        return date[index]-other.date[index];
     }
 }
